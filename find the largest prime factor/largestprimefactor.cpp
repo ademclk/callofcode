49a90459f2367b1e6094d7515a3ea406e1ea38ce
ankitgujral7 <agujral_be19@thapar.edu>
2022-08-16 02:09:27 +0530

    To find the largest prime factor

---
+++ find the largest prime factor/largestprimefactor.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  long long int n=800851475143;
+  long long int max = INT_MIN;
+  while(n%2==0)
+  {
+    max=2;
+    n=n/2;
+  }
+  
+  for(long long int i=3;i<=sqrt(n);i=i+2)//i will only iterate through odd values
+  {
+    while(n%i==0)
+    {
+      max=i;
+      n=n/i;
+    }
+  }
+  if(n>2)
+  {
+    max=n;
+  }
+  cout<<max<<endl;
+}
