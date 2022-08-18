74cdf53f991131a16ad2b4933cd6c2ad5fb5bbaa
babboe1 <ayodelaustinab@gmail.com>
2022-08-18 09:35:44 +0100

    fix: Fix result not returning largest palindrome

--- challenge/largest_palindrome_product/largest_palindrome.js
+++ challenge/largest_palindrome_product/largest_palindrome.js
@@ -1,6 +1,6 @@
 // declare global variables
 let product = 0;
-let palindrome;
+let palindromeArray = [];
 
 const palindromeFunctionHandler = () => {
 	// Since the largest 3-digit number is 999 and the lowest is 100
@@ -8,26 +8,25 @@ const palindromeFunctionHandler = () => {
 
 	for (let num1 = 999; num1 > 99; num1--) {
 		// first loop multiplies num1 by every value of num2 in second loop
-		// for code efficiency, loops run in descending order to get the first palindrome which will be the largest
 
 		for (let num2 = 999; num2 > 99; num2--) {
-         product = num1 * num2;
+			product = num1 * num2;
 
-         // convert number to array in other to reverse
+			// convert number to array in other to reverse
 			let number = product.toString().split('');
 			let numberReversed = [...number].reverse();
 
-         // write condition to check if number is a palindrome eg abcde === edcba
+			// write condition to check if number is a palindrome eg abcde === edcba
 			if (number.join('') === numberReversed.join('')) {
-				palindrome = number.join('');
-
-				console.log(num1, num2);      //995 583
-				console.log(palindrome);      //580085
-
-				return palindrome;
+				//add palindrome to array
+				palindromeArray.push(+number.join(''));
 			}
 		}
 	}
+
+	// get the max of all palindrome
+	const palindrome = Math.max(...palindromeArray);
+	console.log(palindrome);
 };
 
 palindromeFunctionHandler();
