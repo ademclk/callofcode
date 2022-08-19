fbfdf10f76bd73d66d9cd713a05085c256d9903b
ademclk <ademonurcelik@icloud.com>
2022-08-19 14:59:39 +0300

    Moved a file and fixed a typo.

--- challenge/difference_of_the_squares/differenceOfSquares.cpp
+++ challenge/difference_of_the_squares/differenceOfSquares.cpp
@@ -8,7 +8,7 @@ int main(){
     //using the formula to get the sum of squares of first n natural numbers => sumOfSquares = ((n)*(n+1)*(2n+1))/6
     sumOfSquares = ((n)*(n+1)*(2*n+1))/6;
     //using formula to get sum of first n naturals ( sumOfNumbers = (n*(n+1))/2 ), and then squaring it to get squareOfSum
-    int sumOfNums = ((n)*(n+1))/2
+    int sumOfNums = ((n)*(n+1))/2;
     squareOfSum = sumOfNums*sumOfNums;
     cout<<"Sum of squares of n Numbers: "<<sumOfSquares<<endl;
     cout<<"Square of sum of n Numbers: "<<squareOfSum<<endl;
