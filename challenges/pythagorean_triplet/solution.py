dae40729411ce3b899c9346bb500e1d09ee36c98
upakarp <upakarpaudel96@gmail.com>
2022-08-22 23:37:47 -0700

    slight optimization

--- challenges/pythagorean_triplet/solution.py
+++ challenges/pythagorean_triplet/solution.py
@@ -1,6 +1,6 @@
 def pythagorean_triplet(num):
-    for a in range(1, num+1):
-        for b in range(a+1, num+1):
+    for a in range(1, num // 3 + 1):
+        for b in range(a+1, num // 2 + 1):
             c = num - a - b
             if a**2 + b**2 == c**2:
                 return (a, b, c)
