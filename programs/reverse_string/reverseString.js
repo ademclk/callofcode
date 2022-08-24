e3a561ac0d55b8e4d8299a05ecfcae5ba948734b
Ming Ng <69211773+redconOne@users.noreply.github.com>
2022-08-24 13:05:01 -0600

    Solution for reverseString with user prompt added

--- programs/reverse_string/reverseString.js
+++ programs/reverse_string/reverseString.js
@@ -1,3 +1,7 @@
-var reverseString = (str) => {
-  return str.split('').reverse().join('');
+var reverseString = () => {
+  let str = prompt('Please enter a string to be reversed:', 'dlroW olleH');
+
+  console.log(str.split('').reverse().join(''));
 };
+
+reverseString();
