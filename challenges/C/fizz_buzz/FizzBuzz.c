8134d04952bda8b4ffcd47b05ed3be05461a9743
Prince V <princev2487@gmail.com>
2022-08-26 20:29:40 +0530

    Add FizzBuzz (in C)

---
+++ challenges/C/fizz_buzz/FizzBuzz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+	int i = 1, n = 100;
+
+	for(; i <= n; i++) {
+		if(i % 3 == 0 && i % 5 == 0) {
+			printf("FizzBuzz\n");
+		}
+		else if(i % 3 == 0) {
+			printf("Fizz\n");
+		}
+		else if(i % 5 == 0) {
+			printf("Buzz\n");
+		}
+		else {
+			printf("%d\n", i);
+		}
+	}
+
+	return 0;
+}
