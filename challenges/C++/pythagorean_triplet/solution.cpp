8690b8823cfba59fbed2dbb36bc0e8c09cebbdcc
Legoeggolas <55898978+Legoeggolas@users.noreply.github.com>
2022-08-28 21:42:19 +0530

    Optimize the solution for #37

--- challenges/C++/pythagorean_triplet/solution.cpp
+++ challenges/C++/pythagorean_triplet/solution.cpp
@@ -8,10 +8,20 @@ std::array<int, 3> pythaTriplet() {
     std::array<int, 3> triplet = {0, 0, 0};
     bool found = false;
     // since a + b + c = 1000
+    // and a < b < c
     // the max we can go is a = 332, b = 333, c = 334
     for (int a = 1; a <= 332 && !found; a++) {
-        for (int b = a + 1; b <= 333 + (332 - a) && !found; b++) {
+        // the maximum b can reach is when a = 1
+        // then b = 999 - c_min
+        // and since b < c they must be consecutive numbers
+        // which nets b = 499
+        for (int b = a + 1; b <= 499 && !found; b++) {
             int c = 1000 - (a + b);
+
+            // b < c always
+            if (c <= b) {
+                break;
+            }
 
             if (sq(a) + sq(b) == sq(c)) {
                 triplet = {a, b, c};
